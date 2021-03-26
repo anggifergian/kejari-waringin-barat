9266399a434fb8e585ef1ca468cb88c1504f7d67
anggifergian <anggyfergian56@gmail.com>
2021-03-26 09:07:24 +0700

    Separate header & footer

--- wp-content/themes/kejari-waringin/front-page.php
+++ wp-content/themes/kejari-waringin/front-page.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kejari Kota Waringin Barat</title>
+<?php
+get_header();
+?>
 
-    <?php
-    wp_head();
-    ?>
-</head>
-<body>
-
-    <?php
-    wp_footer();
-    ?>
-</body>
-</html>
+<?php
+get_footer();
+?>
