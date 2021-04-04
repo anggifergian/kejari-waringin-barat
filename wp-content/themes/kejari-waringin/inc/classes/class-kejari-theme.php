ec1b7a28fbe4f908d89f23cde09254393223acab
anggifergian <anggyfergian56@gmail.com>
2021-04-04 22:30:47 +0700

    Moving styles & scripts to class-kejari-theme

--- wp-content/themes/kejari-waringin/inc/classes/class-kejari-theme.php
+++ wp-content/themes/kejari-waringin/inc/classes/class-kejari-theme.php
@@ -23,9 +23,76 @@ class KEJARI_THEME
 
     public function setup_theme()
     {
-        /**
-         * Wordpress document title
-         */
+
         add_theme_support('title-tag');
+        add_action('wp_enqueue_scripts', [$this, 'kejari_register_styles']);
+        add_action('wp_enqueue_scripts', [$this, 'kejari_register_scripts']);
+    }
+
+    public function kejari_register_styles()
+    {
+        /**
+         * Custom Css
+         * @version 1.0.0
+         * */
+        $version = wp_get_theme()->get('Version');
+        wp_enqueue_style(
+            'kejari-style',
+            KEJARI_DIR_URI . '/style.css',
+            ['kejari-bootstrap'],
+            $version,
+            'all'
+        );
+
+        /**
+         * Bootstrap CDN
+         * @version 4.6.0
+         * */
+        wp_enqueue_style(
+            'kejari-bootstrap',
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+            [],
+            '4.6.0',
+            'all'
+        );
+    }
+
+    function kejari_register_scripts()
+    {
+        /**
+         * JQuery CDN
+         * @version 3.5.1
+         */
+        wp_enqueue_script(
+            'kejari-jquery',
+            'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+            [],
+            '3.5.1',
+            true
+        );
+
+        /**
+         * Bootstrap CDN
+         * @version 4.6.0
+         */
+        wp_enqueue_script(
+            'kejari-bootstrap',
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js',
+            [],
+            '4.6.0',
+            true
+        );
+
+        /**
+         * Custom Js
+         * @version 1.0
+         */
+        wp_enqueue_script(
+            'kejari-main',
+            KEJARI_DIR_URI . '/assets/js/main.js',
+            [],
+            '1.0',
+            true
+        );
     }
 }
